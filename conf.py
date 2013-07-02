1b78dbddba8b79c36650ae461370e1ec890882bc
Daniel Kettle <daniel.kettle@sait.ca>
2013-07-02 17:07:45 -0600

    drep-repo method for creating directory structure in the works
    Added conf.py configuration file and added code to libdrep

---
+++ conf.py
@@ -0,0 +1,9 @@
+import os
+PATH                = os.environ['HOME']
+COMPONENTS          = ['main', 'non-free', 'contrib']
+ARCH                = ['i386', 'amd64', 'armel', 'armhf']
+
+# Has to be compatible with debian-based Operating Systems
+# Works for Mint 14
+DISTRIBUTION        = [open('/etc/os-release', 'r').readlines()[4].split()[1]]
+
