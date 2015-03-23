d406b63e6f7a4bccbbd1db52fad36e0f3c6d3e43
Daniel Kettle <initial.dann@gmail.com>
2015-03-23 11:26:00 -0700

    update action uses entire path, not package name. Added in armv71->armel for beaglebone architecture

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 ARCH = {"x86_64": "amd64",
-        "x86": "i386"}
+        "x86": "i386",
+        "armv71": "armel"} # Beaglebone black arch
 
 def get_arch(arch):
     return ARCH.get(arch, arch)
