0742b54e57be4f77f59985ac1d66e20727d0985d
Daniel Kettle <initial.dann@gmail.com>
2015-03-27 14:10:04 -0700

    Fixed rem/remove action for files in a package, modularized some db code.

--- lib/db.py
+++ lib/db.py
@@ -13,6 +13,15 @@ def init_package(path, pkgname, **kwargs):
     if os.path.exists(os.path.join(path, pkgname)):
         update_package(os.path.join(path, pkgname), **kwargs)
         
+        
+def __write_db(path, db):
+    try:
+        with open(os.path.join(path, APTPKGDB), 'w') as f:
+            f.write(json.dumps(db, default=str))
+            
+    except Exception as E:
+        print(E)
+
 def read_package(path):
     db = json.dumps({})
     if os.path.exists(os.path.join(path, APTPKGDB)):
@@ -21,19 +30,33 @@ def read_package(path):
             
     return json.loads(db)
 
-def update_package(path, **kwargs):
+def update_package(path, clear_files=False, **kwargs):
+    print(kwargs.get('files'))
     db = read_package(path)
     files = db.get('files', {})
+    print(files)
     if not kwargs.get('files') is None and isinstance(kwargs.get('files'), dict):
         files.update(kwargs['files'])
         
     db.update(kwargs)
-    db['files'] = files
-    try:
-        with open(os.path.join(path, APTPKGDB), 'w') as f:
-            f.write(json.dumps(db, default=str))
-            
-    except Exception as E:
-        print(E)
+    db['files'] = files if not clear_files else {}
+    
+    __write_db(path, db)
         
+    return db
+
+def remove_files(path, *files):
+    db = read_package(path)
+    oldfiles = db.get('files', {})
+    keys = oldfiles.keys()
+    for f in files:
+        print(f)
+        if f in keys:
+            print("Removing {}".format(f))
+            del oldfiles[f]
+            
+    db['files'] = oldfiles # now newfiles
+    print(db['files'])
+    __write_db(path, db)
+    
     return db
