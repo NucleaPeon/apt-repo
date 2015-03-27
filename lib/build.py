4ee52cb045a65cc8f624455ffc1bc607a7facd91
Daniel Kettle <initial.dann@gmail.com>
2015-03-27 12:28:18 -0700

    Fixed installed size calculations, ignores DEBIAN folders, migrated many methods into db and build libraries. Use temporary folders for building debian packages exclusively.

--- lib/build.py
+++ lib/build.py
@@ -8,16 +8,32 @@ from subprocess import Popen, PIPE
 import tempfile
 import shutil
 import os
+import importlib
+from decimal import *
+
+
+
+SECTIONS = ["admin", "cli-mono", "comm", "database", "debug", "devel", "doc", "editors", "education", "electronics", 
+    "embedded", "fonts", "games", "gnome", "gnu-r", "gnustep", "graphics", "hamradio", "haskell", "httpd", "interpreters", "introspection",
+    "java", "kde", "kernel", "libdevel", "libs", "lisp", "localization", "mail", "math", "metapackages", "misc", "net",
+    "news", "ocaml", "oldlibs", "otherosfs", "perl", "php", "python", "ruby", "science", "shells", "sound", "tasks",
+    "tex", "text", "utils", "vcs", "video", "web", "x11", "xfce", "zope"]
 
 def create_deb_struct(path, pkgname, data={}):
     tmpdir = tempfile.mkdtemp(suffix='tmp', prefix=pkgname, 
                               dir=os.path.join(path, pkgname))
+    
     copypath = os.path.join(path, pkgname, tmpdir)
-    shutil.copytree(os.path.join(path, pkgname, 'DEBIAN'), os.path.join(copypath, 'DEBIAN'))
+    os.makedirs(os.path.join(copypath, 'DEBIAN'), exist_ok=True)
     for src, dst in data.get('files', {}).items():
-        os.makedirs(os.path.join(copypath, dst), exist_ok=True)
-        shutil.copytree(src, os.path.join(copypath, dst)) if os.path.isdir(src) \
-            else shutil.copy2(src, os.path.join(copypath, dst))
+        if os.path.isdir(src):
+            shutil.copytree(src, os.path.join(path, pkgname, tmpdir, dst))
+            
+        else:
+            dstpath = os.path.join(path, pkgname, tmpdir, *dst.split(os.sep)[:-1])
+            if not os.path.exists(dstpath):
+                os.makedirs(dstpath, exist_ok=True)
+            shutil.copy2(src, os.path.join(path, pkgname, tmpdir, dst))
         
     return tmpdir
     
@@ -34,9 +50,82 @@ def build_package(path, pkgname, data={}):
                   If directory: saves package based on control file data (name_ver_arch.deb)
                   If filename: save package file to filename
     """
-    print("Deploying Files to temporary build dir")
+    
+    dbmod = importlib.import_module('lib.db')
+    db = dbmod.read_package(os.path.join(path, pkgname))
+    
     tmpdir = create_deb_struct(path, pkgname, data)
+    write_control_file(os.path.join(path, tmpdir), pkgname, **db)
     proc = Popen(['dpkg-deb', '--build', os.path.join(path, tmpdir), "."])
     proc.communicate()
     shutil.rmtree(tmpdir)
     return proc.returncode
+
+def pkg_installed_size(path, append_bytes=None, ignored_files=[".apt-pkg.db"]):
+    """
+    FIXME: Doesn't calculate size recursively
+    :Description:
+        Recursive method that aims to compile a total Installed-Size value
+        for the resulting control file.
+        Performs a quick check to ensure the DEBIAN folder is not included.
+    """
+    bytecount = 0 if append_bytes is None else append_bytes
+    if os.path.exists(path):        
+        if os.path.isdir(path):
+            for root, dirs, files in os.walk(path):
+                if root.split(os.sep)[-1].upper() == 'DEBIAN':
+                    continue
+                    
+                for f in files:
+                    bytecount += pkg_installed_size(f, bytecount) if os.path.isdir(f) else os.path.getsize(f)
+            
+        else:
+            bytecount += os.path.getsize(path)
+    
+    return bytecount
+
+def apt_repo_pkg(path):
+    import lib.db
+    return os.path.exists(os.path.join(path, lib.db.APTPKGDB))
+
+def has_deb_structure(path):
+    if os.path.exists(path) and os.path.isdir(path):
+        for f in os.listdir(path):
+            if f.upper() == "DEBIAN":
+                testfile = os.path.join(path, f, "control")
+                return os.path.isfile(testfile) and os.path.exists(testfile)
+            
+    return False
+
+def write_control_file(path, pkgname, **kwargs):
+    
+    umask = os.umask(0o022)
+    # TODO Detect if package has a database file and use those
+    # Create control file with any data we have to work with:
+    with open(os.path.join(path, "DEBIAN", "control"), 'w') as cf:
+        cf.write("Package: {}\n".format(pkgname))
+        cf.write("Version: {}\n".format(kwargs.get('version', 0.1)))
+        cf.write("Architecture: {}\n".format(' '.join(kwargs.get('architecture'))))
+        cf.write("Section: {}\n".format(kwargs.get('section') if kwargs.get('section') in SECTIONS else "misc"))
+        cf.write("Essential: {}\n".format("yes" if kwargs.get('essential') else "no"))
+        if kwargs.get('depends'):
+            cf.write("Depends: {}\n".format(', '.join(kwargs.get('depends'))))
+            
+        if kwargs.get('recommends'):
+            cf.write("Recommends: {}\n".format(', '.join(kwargs.get('recommends'))))
+        
+        if kwargs.get('suggests'):
+            cf.write("Suggests: {}\n".format(', '.join(kwargs.get('suggests'))))
+        
+        cf.write("Provides: {}\n".format(', '.join(kwargs.get('provides', [pkgname]))))
+        size = Decimal(pkg_installed_size(path) / 1024).quantize(Decimal('1.'), rounding=ROUND_UP)
+        cf.write("Installed-Size: {}\n".format(size))
+        if not kwargs.get('homepage') is None:
+            cf.write("Homepage: {}\n".format(kwargs.get('homepage')))
+        cf.write("Package-Type: deb\n")
+        if not kwargs.get('maintainer') is None:
+            cf.write("Maintainer: {}\n".format(', '.join(kwargs.get('maintainer'))))
+            
+        cf.write("Description: {}\n".format(kwargs.get('desc', 'Description Not Set')))
+        for d in kwargs.get('description', ['...']):
+            cf.write(" {}\n".format(d))
--- lib/db.py
+++ lib/db.py
@@ -11,18 +11,18 @@ def init_repo(**kwargs):
 
 def init_package(path, pkgname, **kwargs):
     if os.path.exists(os.path.join(path, pkgname)):
-        update_package(path, pkgname, **kwargs)
+        update_package(os.path.join(path, pkgname), **kwargs)
         
-def read_package(path, pkgname):
+def read_package(path):
     db = json.dumps({})
-    if os.path.exists(os.path.join(path, pkgname, APTPKGDB)):
-        with open(os.path.join(path, pkgname, APTPKGDB), 'r') as f:
+    if os.path.exists(os.path.join(path, APTPKGDB)):
+        with open(os.path.join(path, APTPKGDB), 'r') as f:
             db = f.read()
             
     return json.loads(db)
 
-def update_package(path, pkgname, **kwargs):
-    db = read_package(path, pkgname)
+def update_package(path, **kwargs):
+    db = read_package(path)
     files = db.get('files', {})
     if not kwargs.get('files') is None and isinstance(kwargs.get('files'), dict):
         files.update(kwargs['files'])
@@ -30,7 +30,7 @@ def update_package(path, pkgname, **kwargs):
     db.update(kwargs)
     db['files'] = files
     try:
-        with open(os.path.join(path, pkgname, APTPKGDB), 'w') as f:
+        with open(os.path.join(path, APTPKGDB), 'w') as f:
             f.write(json.dumps(db, default=str))
             
     except Exception as E:
