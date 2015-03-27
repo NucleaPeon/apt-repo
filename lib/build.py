eb8633e63926df46080d09d022c7cd3674ab1d61
Daniel Kettle <initial.dann@gmail.com>
2015-03-27 09:50:25 -0700

    Update add, create and deletes are working with a database per package. Some tweaking may need to be done, but this should automate a lot.

--- lib/build.py
+++ lib/build.py
@@ -5,8 +5,24 @@
 """
 
 from subprocess import Popen, PIPE
+import tempfile
+import shutil
+import os
 
-def build_package(path, target="."):
+def create_deb_struct(path, pkgname, data={}):
+    tmpdir = tempfile.mkdtemp(suffix='tmp', prefix=pkgname, 
+                              dir=os.path.join(path, pkgname))
+    copypath = os.path.join(path, pkgname, tmpdir)
+    shutil.copytree(os.path.join(path, pkgname, 'DEBIAN'), os.path.join(copypath, 'DEBIAN'))
+    for src, dst in data.get('files', {}).items():
+        os.makedirs(os.path.join(copypath, dst), exist_ok=True)
+        shutil.copytree(src, os.path.join(copypath, dst)) if os.path.isdir(src) \
+            else shutil.copy2(src, os.path.join(copypath, dst))
+        
+    return tmpdir
+    
+
+def build_package(path, pkgname, data={}):
     """
     TODO:
         - Copy package into a temporary directory where it can be built either in a 
@@ -18,6 +34,9 @@ def build_package(path, target="."):
                   If directory: saves package based on control file data (name_ver_arch.deb)
                   If filename: save package file to filename
     """
-    proc = Popen(['dpkg-deb', '--build', path, target])
+    print("Deploying Files to temporary build dir")
+    tmpdir = create_deb_struct(path, pkgname, data)
+    proc = Popen(['dpkg-deb', '--build', os.path.join(path, tmpdir), "."])
     proc.communicate()
+    shutil.rmtree(tmpdir)
     return proc.returncode
--- lib/db.py
+++ lib/db.py
@@ -10,21 +10,25 @@ def init_repo(**kwargs):
     pass
 
 def init_package(path, pkgname, **kwargs):
-    update_package(path, pkgname, **kwargs)
+    if os.path.exists(os.path.join(path, pkgname)):
+        update_package(path, pkgname, **kwargs)
         
-def read_package(path, pkgname, **kwargs):
-    db = {}
-    try:
+def read_package(path, pkgname):
+    db = json.dumps({})
+    if os.path.exists(os.path.join(path, pkgname, APTPKGDB)):
         with open(os.path.join(path, pkgname, APTPKGDB), 'r') as f:
             db = f.read()
             
-    except Exception as E:
-        print(E)
-        
     return json.loads(db)
 
 def update_package(path, pkgname, **kwargs):
-    db = kwargs
+    db = read_package(path, pkgname)
+    files = db.get('files', {})
+    if not kwargs.get('files') is None and isinstance(kwargs.get('files'), dict):
+        files.update(kwargs['files'])
+        
+    db.update(kwargs)
+    db['files'] = files
     try:
         with open(os.path.join(path, pkgname, APTPKGDB), 'w') as f:
             f.write(json.dumps(db, default=str))
