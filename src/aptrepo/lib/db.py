76cc3320bf7dbd6fb75001b61c58dac5af7bf17f
Daniel Kettle <initial.dann@gmail.com>
2015-05-05 14:22:19 -0700

    Changed database to read/write to configparser files

--- src/aptrepo/lib/db.py
+++ src/aptrepo/lib/db.py
@@ -9,13 +9,41 @@ import json
 from aptrepo.lib.arch import arch
 import socket
 
+# Define how configparser values get placed back into python objects here:
+LEGEND = {
+    'Package': {
+        'set_version': lambda x: int(x),
+        'provides': lambda x: x.split(','),
+        'directory': lambda x: os.path.join(x),
+        'is_essential': lambda x: x == "True",
+        'desc': lambda x: str(x),
+        'description': lambda x: '\n . \n'.join(x.split(' . ')),
+        'depends': lambda x: x.split(','),
+        'suggests': lambda x: x.split(','),
+        'section': lambda x: str(x),
+        'architecture': lambda x: x.split(',')
+        },
+    'Build': {
+        'profile': lambda x: x.split(',')
+        },
+    'Override': lambda x, y: {x: y},
+    'Files': lambda x, y: {x: y},
+    'Control': lambda x, y: {x: y},
+    'User': {'author': lambda x: str(x),
+             'maintainer': lambda x: x.split(','),
+             'homepage': lambda x: str(x)}
+    }
+
 class PackageDB():
     
     PROFILES = ["deb", "opk", "ipk", "tar.gz"]
+    SECTIONS = ['Package', 'Build', 'Override', 'Files', 'Control', 'User']
     
     def __init__(self, path, pkgname, **kwargs):
-        self.DB_STRUCT = {
-            'Package' : {
+        
+        self.pkgname = pkgname
+        self.db = {
+            'Package': {
                 'set_version': kwargs.get('set_version', "0.1"),
                 'provides': kwargs.get('provides', [pkgname]),
                 'directory': kwargs.get('directory', os.path.abspath(os.getcwd())),
@@ -26,24 +54,29 @@ class PackageDB():
                 'suggests': kwargs.get('suggests', []),
                 'section': kwargs.get('section', "misc"),
                 'architecture': kwargs.get('architecture', arch())},
-            'Build'   : {'profiles': 'deb'},
+            'Build': {'profiles': 'deb'},
             'Override': {k: v for k, v in kwargs.get('override', {}).items()},
-            'Control' : {k: v for k, v in kwargs.get('control', {}).items()},
-            'Files'   : {k: v for k, v in kwargs.get('files', {}).items()},
-            'User'    : {'author': kwargs.get('author', ""),
-                         'maintainer': kwargs.get('maintainer', [socket.gethostname()]),
-                         'homepage': kwargs.get('homepage', '')}
-            }
-        self.pkgname = pkgname
+            'Files': {k: v for k, v in kwargs.get('files', {}).items()},
+            'Control': {k: v for k, v in kwargs.get('control', {}).items()},
+            'User': {'author': kwargs.get('author', ""),
+                     'maintainer': kwargs.get('maintainer', [socket.gethostname()]),
+                     'homepage': kwargs.get('homepage', '')}}
+            
+
+        
+        # If a database file already exists, overwrite it with saved information
+        if os.path.exists(os.path.join(path, pkgname, pkgname)):
+            print("Reading existing database.")
+            self.read(path, pkgname)
                          
-    def write(self, **kwargs):
+    def write(self):
         config = configparser.ConfigParser()
-        for k, v in self.DB_STRUCT.items():
-            config[k] = {}
+        for k, v in self.db.items():
+            if not k in config:
+                config[k] = {}
+                                
+            # section key-value pairs
             for key, val in v.items():
-                if key in kwargs:
-                    val = kwargs[key]
-                
                 if isinstance(val, list):
                     val = ', '.join(val)
                     
@@ -52,23 +85,54 @@ class PackageDB():
                     
                 if val is None or not val:
                     val = ''
-                    
+
                 config[k][key] = val
                 
-        with open(os.path.join(self.DB_STRUCT['Package']['directory'], 
+        with open(os.path.join(self.db['Package']['directory'], 
                                self.pkgname, self.pkgname), 'w') as f:
             config.write(f)
+            
+            
+    def update(self, **kwargs):
+        # Check if kwargs reflects a key in a section in self.db
+        for k, v in kwargs.items():
+            for s in self.SECTIONS:
+                if k in self.db[s] and not v is None:
+                    print("Updating key {}".format(k))
+                    self.db[s][k] = v
     
-    def read_db(self, *keys):
-        print("read db")
-    
+    def read(self, path, pkgname):
+        """ Reads sections found in SECTIONS """
+        config = configparser.ConfigParser()
+        config.read(os.path.join(path, pkgname, pkgname))
+        
+        for section in self.SECTIONS:
+            for key in config[section]:
+                if not isinstance(LEGEND[section], dict):
+                    print(LEGEND[section])
+                    self.db[section].update(LEGEND[section](key, config[section][key]))
+                    
+                else:
+                    self.db[section][key] = config[section][key]
+                               
     def validate(self):
-        print("validate db")
+        config = configparser.ConfigParser()
+        config.read(os.path.join(self.db['Package']['directory'], self.pkgname, self.pkgname))
+        for s in config.sections():
+            if not s in self.SECTIONS:
+                return False
+            
         return True
     
     def duplicate(self, path, pkgname):
         ''' Includes some refactoring '''
         print("duplicate db")
+    
+    def json(self):
+        return json.dumps(self.db, default=str,
+                             sort_keys=True,
+                             indent=4,
+                             separators=(',', ': ',))
     
     def __str__(self):
         return str(self.DB_STRUCT)
